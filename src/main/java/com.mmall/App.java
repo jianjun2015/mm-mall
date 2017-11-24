bc03dfb22443a1d2861ee2bd27e45b8e1285a4a3
jianjun2015 <jianjun_info@163.com>
2017-11-24 11:41:37 +0800

    'mod'

---
+++ src/main/java/com.mmall/App.java
@@ -0,0 +1,7 @@
+package com.mmall;
+
+/**
+ * Created by wangjianjun on 2017/11/24.
+ */
+public class App {
+}
---
+++ src/test/java/com/mmall/AppTest.java
@@ -0,0 +1,10 @@
+package com.mmall;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by wangjianjun on 2017/11/24.
+ */
+public class AppTest extends TestCase {
+
+}
