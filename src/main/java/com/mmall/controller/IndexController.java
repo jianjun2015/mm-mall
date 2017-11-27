2fa73f9eb07c136ad2438be303b2cd7e6b972033
jianjun2015 <jianjun_info@163.com>
2017-11-27 18:01:39 +0800

    '添加日志信息'

--- src/main/java/com/mmall/controller/IndexController.java
+++ src/main/java/com/mmall/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.mmall.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,10 +12,20 @@ import javax.servlet.http.HttpServletResponse;
  * Created by wangjianjun on 2017/11/24.
  */
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping("/")
+    Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("index")
     public String login(HttpServletRequest request, HttpServletResponse response){
+
+        logger.trace("index controller trace");
+        logger.debug("index controller debug");
+        logger.info("index controller info");
+        logger.warn("index controller warn");
+        logger.error("index controller error");
+
         return "index";
     }
 }
